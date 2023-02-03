f7cbca7770292d784674736ee131cbd3c5370a6b
Chekameeva <anastasiya-parsek@mail.ru>
2023-02-03 13:55:50 +0300

    homework3

--- Homework/Lesson2/2_1.py
+++ Homework/Lesson2/2_1.py
@@ -11,4 +11,9 @@ for x in range(0, 1000):
     for y in range(0, 1000):
         if (x+y == s and x * y == p):
             print(x, y)
+
+# num = {k: v ** 3 for k, v in zip("ASDFGHJK", range(1, 11))}
+
+# print(num)
+
     
